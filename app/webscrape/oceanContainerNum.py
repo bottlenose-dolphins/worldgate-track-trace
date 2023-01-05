f36a5c4d9bce4c930e690f0cc38bbb7db77b4b26
VaruneshBala <balamuniap.2019@smu.edu.sg>
2023-01-05 20:46:26 +0800

    made changes according to pull request comments

--- app/webscrape/oceanContainerNum.py
+++ app/webscrape/oceanContainerNum.py
@@ -1,15 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-from fake_useragent import UserAgent
 import time
 
-containerNum = "GAOU6627318"
-blNum = "SINC72210300"
+identifier = "GAOU6627318"
 
 try:
+
+    # headless mode will run the webscraping in the background and lesser RAM will be utilised --> Much more efficient process
 
     options = Options()
 
@@ -21,16 +19,24 @@ try:
 
     driver.maximize_window()
 
-    driver.get('https://ecomm.one-line.com/one-ecom/manage-shipment/cargo-tracking?ctrack-field=' + containerNum + '&trakNoParam=' + containerNum)
+    # Can interchange identifier with Container/BL Number as the process is similar
+
+    driver.get('https://ecomm.one-line.com/one-ecom/manage-shipment/cargo-tracking?ctrack-field=' + identifier + '&trakNoParam=' + identifier)
 
     time.sleep(3)
 
+    # pop-up present when querying for status in the page so need to click "Skip"
+
     driver.find_element(By.XPATH, '//*[@id="headlessui-popover-panel-28"]/div/div[2]/div[3]/button[1]').click()
+
+    # Inline Frame present so need to change to this so you can extract values
 
     driver.switch_to.frame("IframeCurrentEcom")
 
     status = driver.find_element(By.XPATH, '//*[@id="1"]/td[9]').text
-    
+
+    #clicking the Container Number will show us more details like Port of Destination and ETA
+
     driver.find_element(By.XPATH, '//*[@id="1"]/td[4]/a').click()
 
     time.sleep(10)
