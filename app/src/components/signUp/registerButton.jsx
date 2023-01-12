f325d08442385037674c6227384c435c7f58e9d5
Jeffvinder Singh <jeffvinder@hotmail.com>
2023-01-12 18:32:49 +0800

    Created Modal Pop Up for Registration

--- app/src/components/signUp/registerButton.jsx
+++ app/src/components/signUp/registerButton.jsx
@@ -6,21 +6,27 @@ const RegisterButton = ({ handleClick }) => {
   
     return (
     <>
-    <button className="mt-5 place-self-end bg-blue-500 hover:bg-blue-400 text-white font-medium py-2 px-4 rounded-lg justify-end" onClick={() => setModalIsOpen(true)}>
-    Register
+    <button className="mt-5 place-self-end bg-green-600 hover:bg-green-500 text-white font-medium py-2 px-4 rounded-lg justify-end" onClick={() => setModalIsOpen(true)}>
+    Sign Up
     </button>
     <Modal
-    isOpen={modalIsOpen}
-    onRequestClose={() => setModalIsOpen(false)}
+      isOpen={modalIsOpen}
+      onRequestClose={() => setModalIsOpen(false)}
+      className="bg-white rounded-lg p-8"
+      overlayClassName="fixed inset-3 z-50"
     >
-    <h2>Confirm Registration</h2>
-    <p>Are you sure you want to register?</p>
-    <div>
-    <button onClick={handleClick}>Confirm</button>
-    <button onClick={() => setModalIsOpen(false)}>Cancel</button>
-    </div>
-    </Modal>
-    </>
+      <h2 className="text-lg font-medium">Registration Successful!</h2>
+      <p className="text-gray-700">Your account has been created in our system and you are able to use Track & Trace</p>
+      <div className="flex justify-end mt-4">
+        <button 
+          className="bg-blue-500 hover:bg-blue-400 text-white font-medium py-2 px-4 rounded-full"
+          onClick={() => setModalIsOpen(false)}
+        >
+          Confirm
+        </button>
+          </div>
+          </Modal>
+          </>
     );
 }
 
