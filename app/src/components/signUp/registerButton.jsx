e4b5c41c20959c3ff6be25c3c79019812daba121
Jeffvinder Singh <jeffvinder@hotmail.com>
2023-01-12 22:07:48 +0800

    Updated UI to match Figma

--- app/src/components/signUp/registerButton.jsx
+++ app/src/components/signUp/registerButton.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
+import modalImg from "../../img/RegisterModalImg.png";
 
 const RegisterButton = ({ handleClick }) => {
     const [modalIsOpen, setModalIsOpen] = useState(false);
+    const toggleModal = () => {
+        setModalIsOpen(!modalIsOpen);
+      }
   
     return (
     <>
@@ -10,22 +14,23 @@ const RegisterButton = ({ handleClick }) => {
     Sign Up
     </button>
     <Modal
-      isOpen={modalIsOpen}
-      onRequestClose={() => setModalIsOpen(false)}
-      className="bg-white rounded-lg p-8"
-      overlayClassName="fixed inset-3 z-50"
+        isOpen={modalIsOpen}
+        onRequestClose={toggleModal}
+        className="w-3/4 h-3/4 mx-auto my-auto flex flex-col items-center justify-center p-4 rounded-lg"
+        overlayClassName="fixed inset-0 z-50 bg-white w-3/12 h-1/2 mx-auto my-auto"
+        data-modal-backdrop="static"
     >
-      <h2 className="text-lg font-medium">Registration Successful!</h2>
-      <p className="text-gray-700">Your account has been created in our system and you are able to use Track & Trace</p>
-      <div className="flex justify-end mt-4">
-        <button 
-          className="bg-blue-500 hover:bg-blue-400 text-white font-medium py-2 px-4 rounded-full"
-          onClick={() => setModalIsOpen(false)}
-        >
-          Confirm
-        </button>
-          </div>
-          </Modal>
+        <img src={modalImg} alt="modal-image" className="w-24 h-24 rounded-full mt-auto" />
+        <h2 className="text-lg font-medium mt-4">Registration Successful</h2>
+        <div className="flex justify-center mt-4">
+            <button 
+            className="bg-green-600 hover:bg-green-500 text-white font-medium px-8 py-2 rounded-lg"
+            onClick={toggleModal}
+            >
+            Continue to Sign in
+            </button>
+        </div>
+    </Modal>
           </>
     );
 }
