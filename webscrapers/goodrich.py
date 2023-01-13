6742b153d2ae757b913ed93ac624dcf1638b44b0
Brayden Leo <46068652+prussianman@users.noreply.github.com>
2023-01-13 13:02:07 +0800

    made changes in accordance to review

--- webscrapers/goodrich.py
+++ webscrapers/goodrich.py
@@ -1,39 +1,16 @@
 #scraper related
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver import ActionChains
-
-#exceptions from selenium 
-from selenium.common.exceptions import NoSuchElementException
-
-# driver management (either first 2 or third)
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-import chromedriver_binary
 
 #server related
-from flask import Flask, request, jsonify
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
-from flask_cors import CORS
-
-#logging
-import logging
+from flask import Flask, jsonify
 
 # creds
 from creds.goodrich import username
 from creds.goodrich import password
 
 app = Flask(__name__)
-# limiter = Limiter(
-#     app,
-#     key_func=get_remote_address,
-#     default_limits=["2000 per day", "500 per hour"]
-# )
-
-CORS(app)
 
 @app.route("/ping", methods=['GET'])
 def ping():
@@ -48,28 +25,24 @@ def track(tracking_type, tracking_identifier):
     options.add_argument('--disable-gpu')
 
     # init
-    # driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
     driver = webdriver.Chrome(options=options)
 
     # run with:
-    # http://127.0.0.1:5003/GOOD/BL/VASSINCMB015609
-    # http://127.0.0.1:5003/GOOD/CTR/VMLU3817377
+    # http://127.0.0.1:5004/GOOD/BL/VASSINCMB015609
+    # http://127.0.0.1:5004/GOOD/CTR/VMLU3817377
 
     # set max waiting time before throwing an exception
     driver.implicitly_wait(5)
-    # for executing javascript
-    action = ActionChains(driver)
     
     # landing page
     driver.get("https://goodrich.co/track.php#new_tab")
-    # print("reachedLandingSite")
 
     # iframe switch
     try:
         driver.switch_to.frame(driver.find_element(by=By.XPATH, value="/html/body/div[1]/iframe"))
         # username and password input elements
-        eleUsername = driver.find_element(by=By.XPATH, value="//*[@id=\"UserId\"]")
-        elePassword = driver.find_element(by=By.XPATH, value="//*[@id=\"sPassword\"]")
+        eleUsername = driver.find_element(by=By.ID, value="UserId")
+        elePassword = driver.find_element(by=By.ID, value="sPassword")
 
         # to replace value attribute w un&pw
         driver.execute_script("arguments[0].value ='" + username + "'", eleUsername)
@@ -78,60 +51,60 @@ def track(tracking_type, tracking_identifier):
         # submit login credentials
         driver.find_element(by=By.XPATH, value="//*[@id=\"login\"]/div/input").click()
         # print("entered result page")
-    
-    except:
-        return("failed to login")
-    
-    try:
+        
         # query and results page
         if(tracking_type == "CTR"):
             driver.find_element(by=By.XPATH, value="//*[@id=\"TAB1\"]/ul/li[2]/a").click()
             # query
             # enter CTR value
-            driver.find_element(by=By.XPATH, value="//*[@id=\"ContainerNo\"]").send_keys(tracking_identifier)
+            driver.find_element(by=By.ID, value="ContainerNo").send_keys(tracking_identifier)
             # click search button
             driver.find_element(by=By.XPATH, value="//*[@id=\"TAB2\"]/div[1]/div/div[3]/input").click()
             
-            destinationPort = driver.find_element(by=By.XPATH, value="//*[@id=\"fpodId2\"]").get_attribute("innerHTML").strip().title()
-            # print(destinationPort)
-            # driver.execute_script("window.scrollBy(0,500)","")
-            vesselName = driver.find_element(by=By.XPATH, value="//*[@id=\"table2id\"]/table[1]/tbody/tr[5]/td[5]").text.strip().title()
-            # print(vesselName)
-            status, est = driver.find_element(by=By.XPATH, value="//*[@id=\"fpodEtaId2\"]").text.split(" : ")
-            formatStatus = status.strip().title()
-
-        else:
+            port_of_discharge = driver.find_element(by=By.XPATH, value="//*[@id=\"table2id\"]/table[1]/tbody/tr[5]/td[5]").text.strip().title()
+            vessel_name= driver.find_element(by=By.XPATH, value="//*[@id=\"table2id\"]/table[1]/tbody/tr[3]/td[2]").text.strip().title()
+            status, arrival_datetime = driver.find_element(by=By.ID, value="fpodEtaId2").text.split(" : ")
+        
+        elif(tracking_type == "BL"):
             # query
             # enter BL value
-            driver.find_element(by=By.XPATH, value="//*[@id=\"BlNo\"]").send_keys(tracking_identifier)
+            driver.find_element(by=By.ID, value="BlNo").send_keys(tracking_identifier)
             # click search button
             driver.find_element(by=By.XPATH, value=" //*[@id=\"TAB1\"]/div[1]/div/div[3]/input").click()
-            destinationPort = driver.find_element(by=By.XPATH, value="//*[@id=\"fpodId\"]").text.strip().title()
-            # print(destinationPort)
-            vesselName = driver.find_element(by=By.XPATH, value="//*[@id=\"table1id\"]/table[1]/tbody/tr[3]/td[2]").text.strip().title()
-            # print(vesselName)
-            status, est = driver.find_element(by=By.XPATH, value="//*[@id=\"fpodEtaId\"]").text.split(" : ")
-            formatStatus = status.strip().title()
+            
+            port_of_discharge = driver.find_element(by=By.XPATH, value="//*[@id=\"table1id\"]/table[1]/tbody/tr[5]/td[5]").text.strip().title()
+            vessel_name = driver.find_element(by=By.XPATH, value="//*[@id=\"table1id\"]/table[1]/tbody/tr[3]/td[2]").text.strip().title()
+            status, arrival_datetime = driver.find_element(by=By.ID, value="fpodEtaId").text.split(" : ")
 
             # print(formatStatus)
             # print(est)
+        
+        arrival_date = arrival_datetime.split()[0]
+        status = status.strip().title()
+        return jsonify(
+                {
+                    "code": 200,
+                    "data": {
+                        "status": status,
+                        "arrival_date": arrival_date,
+                        "port_of_discharge": port_of_discharge,
+                        "vessel_name": vessel_name
+                    }
+                }
+        )
 
-        if formatStatus == "Arrived":
-            return("Status of your query : " + formatStatus + ". It arrived at " + destinationPort + " on " + est)
-
-        # Unsure on wording
-        elif formatStatus == "Arriving":
-            return("Status of your query : " + formatStatus + ". It arrived at " + destinationPort + " on " + est)
-
-        else:
-            return("Status could not be determined")
-
-    except:
-        return("failed to obtain results")
+    except Exception as e:
+        
+        return jsonify(
+            {
+                "code": 500,
+                "message": str(e)
+            }
+        ), 500
     
     finally:
         driver.close()
 
 
 if __name__ == '__main__':
-    app.run(port=5003, debug=True)
+    app.run(port=5004, debug=True)
