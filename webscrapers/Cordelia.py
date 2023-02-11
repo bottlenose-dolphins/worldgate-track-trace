23c6e2922070fcf85a55fb6a99c56e658216723a
charmjj <charmainelimjj@gmail.com>
2023-02-11 13:57:42 +0800

    fix cordelia scraper

--- webscrapers/Cordelia.py
+++ webscrapers/Cordelia.py
@@ -1,19 +1,20 @@
-#scraper related
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 #server related
+# server related
 from flask import Flask, jsonify
 
-# creds
-username=""
-password=""
+import time
 
+username = ""
+password = ""
 app = Flask(__name__)
-
-
-
 
 @app.route("/CCSL/<string:tracking_type>/<string:tracking_identifier>", methods=['GET'])
 def track(tracking_type, tracking_identifier):
@@ -25,15 +26,12 @@ def track(tracking_type, tracking_identifier):
     # init
     driver = webdriver.Chrome(options=options)
 
-  
-
     # set max waiting time before throwing an exception
     driver.implicitly_wait(5)
     
     # landing page
     driver.get("https://www.cordelialine.com/login-and-registration/")
 
-    # iframe switch
     try:
       
         # username and password input elements
@@ -46,66 +44,51 @@ def track(tracking_type, tracking_identifier):
 
         # submit login credentials
         driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div/div/div/div/div[1]/div/div/div[1]/form[1]/div[7]/div/button").click()
-        # print("entered result page")
-        
+        time.sleep(3)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")     
         # query and results page
         if(tracking_type == "BL"):
-            driver.get("https://www.cordelialine.com/service-routes/")
+            driver.get("https://www.cordelialine.com/bltracking/?blno="+tracking_identifier)
+            time.sleep(2)
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")     
 
-            driver.find_element(by=By.XPATH, value="//*[@id=\"showbutton\"]/button[1]").click()
-            # query
-            # enter CTR value
-            driver.find_element(by=By.ID, value="blno1").send_keys(tracking_identifier)
-            # click search button
-            driver.find_element(by=By.XPATH, value="//*[@id=\"bltrackingForm\"]/div/div[2]/button").click()
- 
-            port_of_discharge = driver.find_element(by=By.XPATH, value="//*[@id=\"checkShedTable1\"]/tbody/tr/td[1]").text.strip().title()
-            vessel_name= driver.find_element(by=By.XPATH, value="//*[@id=\"checkShedTable\"]/tbody/tr/td[5]").text.strip().title()
-            status=driver.find_element(by=By.XPATH, value="//*[@id=\"checkShedTable1\"]/tbody/tr/td[12]").text.strip().title()
-            arrival_datetime = driver.find_element(by=By.XPATH, value="//*[@id=\"checkShedTable1\"]/tbody/tr/td[2]").text.strip().title()
-        
+
+            port_of_discharge = driver.find_element(by=By.XPATH, value="//*[@id='checkShedTable1']/tbody/tr/td[1]").text.strip().title()
+            vessel_name = driver.find_element(by=By.XPATH, value="//*[@id='checkShedTable']/tbody/tr/td[5]").text.strip().title()
+            status = driver.find_element(by=By.XPATH, value="//*[@id='checkShedTable1']/tbody/tr/td[12]").text.strip().title()
+            arrival_datetime = driver.find_element(by=By.XPATH, value="//*[@id='checkShedTable1']/tbody/tr/td[2]").text.strip().title()
+
         elif(tracking_type == "CTR"):
-            driver.get("https://www.cordelialine.com/service-routes/")
-
-            driver.find_element(by=By.XPATH, value="//*[@id=\"showbutton\"]/button[1]").click()
-            # query
-            # enter CTR value
-            driver.find_element(by=By.ID, value="blno1").send_keys(tracking_identifier)
-            # click search button
-            driver.find_element(by=By.XPATH, value="//*[@id=\"bltrackingForm\"]/div/div[2]/button").click()
+            driver.get("https://www.cordelialine.com/container-tracking/?contno="+tracking_identifier)
+            time.sleep(2)
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") 
             
-            port_of_discharge = driver.find_element(by=By.XPATH, value="//*[@id=\"checkShedTable1\"]/tbody/tr/td[1]").text.strip().title()
+            port_of_discharge = driver.find_element(by=By.XPATH, value="/html/body/div/div/div/div/div/div/section/table[2]/tbody/tr/td[1]").text.strip().title()
             vessel_name = driver.find_element(by=By.XPATH, value="//*[@id=\"checkShedTable1\"]/tbody/tr/td[6]").text.strip().title()
             status=driver.find_element(by=By.XPATH, value="//*[@id=\"checkShedTable1\"]/tbody/tr/td[12]").text.strip().title()
-            arrival_datetime = driver.find_element(by=By.XPATH, value="//*[@id=\"checkShedTable1\"]/tbody/tr/td[2]").text.strip().title()
-            # print(formatStatus)
-            # print(est)
-        
+            arrival_datetime = driver.find_element(by=By.XPATH, value="/html/body/div/div/div/div/div/div/section/table[2]/tbody/tr/td[2]").text.strip().title()
       
         return jsonify(
-                {
-                    "code": 200,
-                    "data": {
-                        "status": status,
-                        "arrival_date": arrival_datetime,
-                        "port_of_discharge": port_of_discharge,
-                        "vessel_name": vessel_name
-                    }
+            {
+                "code": 200,
+                "data": {
+                    "status": status,
+                    "arrival_date": arrival_datetime,
+                    "port_of_discharge": port_of_discharge,
+                    "vessel_name": vessel_name
                 }
+            }
         )
-
-    except Exception as e:
         
+    except Exception as e:
         return jsonify(
             {
                 "code": 500,
                 "message": str(e)
-            }
-        ), 500
-    
+            })
     finally:
         driver.close()
-
-
+        
+    
 if __name__ == '__main__':
     app.run(port=5004, debug=True)
