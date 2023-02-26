fe690f193b0bd66f0caf80647da9b8a9df93be8a
charmjj <charmainelimjj@gmail.com>
2023-02-26 22:50:44 +0800

    minor UI improvements

--- services/frontend/app/src/components/Home/index.jsx
+++ services/frontend/app/src/components/Home/index.jsx
@@ -11,12 +11,12 @@ export default function Home() {
     navigate("/about");
   }
   return (
-    <div className="bg-image flex flex-col w-full h-full md:flex-row md:space-x-4 md:space-y-0 mt-5 bg-fixed">
-      <div className=' md:w-1/2 lg:w-1/2 ml-40'>
-        <h1 className=' sm:text-3xl md:text-8xl lg:text-8xl font-bold text-white mt-48 mb-2'>Keeping track of</h1>
-        <h1 className=' sm:text-3xl md:text-8xl lg:text-8xl font-bold text-white mb-2'>your shipment</h1>
-        <h1 className=' sm:text-3xl md:text-8xl lg:text-8xl font-bold text-white'>just got easier!</h1>
-        <p className='text-2xl mt-10 mb-10 text-white'>Worldgate introduces <span className='font-bold'>Track&Trace</span>,<br /> for <span className='font-bold'>all</span> your shipment needs.</p>
+    <div className="bg-image flex flex-col w-full h-full md:flex-row md:space-x-4 md:space-y-0 bg-fixed">
+      <div className='md:w-1/2 lg:w-1/2 ml-36'>
+        <h1 className='lg:leading-tight 2xl:leading-tight text-3xl md:text-6xl lg:text-7xl 2xl:text-8xl font-bold text-white lg:mt-10 2xl:mt-40 mb-2'>Keeping track of your shipments just got easier!</h1>
+        {/* <h1 className=' sm:text-3xl md:text-8xl lg:text-8xl font-bold text-white mb-2'>your shipment</h1>
+        <h1 className=' sm:text-3xl md:text-8xl lg:text-8xl font-bold text-white'>just got easier!</h1> */}
+        <p className='text-2xl lg:my-6 2xl:my-10 text-white'>Worldgate introduces <span className='font-bold'>Track&Trace</span>,<br /> for <span className='font-bold'>all</span> your shipment needs.</p>
         <div className='flex'>
           <Link to='/sign-up'>
             <button type="button" onClick={handleClick} className='signup-button mt-2 py-2 px-4'>Sign Up</button>
