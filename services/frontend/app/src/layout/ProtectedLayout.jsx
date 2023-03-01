3d09341cd9d049b998c4ea81523b0533dedfb5d4
charmjj <charmainelimjj@gmail.com>
2023-03-01 16:30:59 +0800

    add ProtectedLayout for protected endpoints

---
+++ services/frontend/app/src/layout/ProtectedLayout.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import NavbarUser from "./NavbarUser";
+import SideBar from "./SideBar";
+
+export default function ProtectedLayout({ redirectPath = "/sign-in" }) {
+    const [username, setUsername] = useState("");
+
+    useEffect(() => { // TODO: refactor.
+        const usernameValue = localStorage.getItem("username");
+        if (typeof usernameValue !== "undefined" && usernameValue && usernameValue !== "") {
+            setUsername(usernameValue);
+        }
+    })
+
+    if (localStorage.getItem("username") === "" || localStorage.getItem("username") === null) {
+        return <Navigate to={redirectPath} replace />;
+    }
+
+    return (
+        <>
+            <NavbarUser username={username} />
+            <SideBar />
+            <Outlet />
+        </>
+    );
+}
---
+++ services/frontend/app/src/layout/SideBar.jsx
@@ -0,0 +1,8 @@
+
+export default function SideBar() {
+    return (
+        <aside>
+            side bar goes here
+        </aside>
+    )
+}
