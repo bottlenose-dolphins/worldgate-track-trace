9a16e57708e0f9921928e4d2105bdf70ea68f3f1
charmjj <charmainelimjj@gmail.com>
2023-03-06 16:22:15 +0800

    refactor and fix UI for ViewShipmentComponent

--- services/frontend/app/src/components/ViewShipments/ViewShipmentComponent.jsx
+++ services/frontend/app/src/components/ViewShipments/ViewShipmentComponent.jsx
@@ -1,76 +1,75 @@
 import React, { useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
 import { getImportShipments, getExportShipments } from "src/api/shipment";
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
 import locationWhite from "../../img/locationWhite.png";
-import sortButton from "../../img/sortButton.png";
 
 export default function ViewShipmentComponent({ title, type }) {
-
-  const [buttonText, setButtonText] = useState("closest to arrival");
+  const [isLatestOnTop, setIsLatestOnTop] = useState(true);
   const [items, setItems] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       if (type === "Import") {
         const importShipments = await getImportShipments();
-        setItems(importShipments.slice().reverse());
+        setItems(importShipments.slice());
       } else if (type === "Export") {
         const exportShipments = await getExportShipments();
-        setItems(exportShipments.slice().reverse());
+        setItems(exportShipments.slice());
       }
     };
     fetchData();
   }, [type]);
 
   const handleClick = () => {
-    if (buttonText === "closest to arrival") {
-      setButtonText("furthest from arrival");
-      setItems(items.slice().reverse());
-    }
-    else {
-      setButtonText("closest to arrival");
-      setItems(items.slice().reverse());
-    }
+    setIsLatestOnTop(!isLatestOnTop);
+    setItems(items.slice().reverse());
   };
 
   return (
-    <div className="p-10">
+    <div className="p-9 w-3/4">
       <h1 className="text-2xl font-medium text-blue-700">{title}</h1>
-      <div className="my-auto bg-light-blue-500 p-4 rounded-lg border border-blue-500 w-1/2">
-        <h2 className="mt-6 text-lg font-medium text-blue-700 inline-block">List view</h2>
-        <img className="ml-80 inline-block" src={sortButton} alt="sort" /> <button type="button" className="font-medium text-blue-700" onClick={handleClick}>{buttonText}</button>
-        <div className="mt-6">
+      <div className="my-auto bg-blue-50 p-4 rounded-lg border border-blue-500">
+
+        <div className="flex justify-between mt-3 mx-1">
+          <div className="text-lg font-medium text-blue-700 w-1/2">List View</div>
+          <button type="button" className="flex justify-end w-1/2 items-center" onClick={handleClick}>
+            <ChevronDownIcon className={`w-6 h-6 text-blue-700 mr-1 ${!isLatestOnTop && "rotate-180"}`} />
+            <div className="font-medium text-blue-700 hover:underline">{ isLatestOnTop ? "closest to arrival" : "furthest from arrival" }</div>
+          </button>
+        </div>
+
+        <div className="flex flex-col mt-5">
           {items.map((item, index) => {
-
             return (
-              <div>
-                <div className="grid grid-cols-2 gap-2">
-                  <div className=" p-2 mt-5">
-
-                    <Card style={{ width: "36rem", backgroundColor: "#217BF4", borderRadius: "10px" }} key={index}>
-                      <Card.Body>
-                        <div className="grid grid-cols-2">
-                          <div className="  ">
-                            <Card.Title className=" font-sans text-white  text-5xl p-6">{item.arrival_date ? item.arrival_date.slice(0, 6) : item.delivery_date.slice(0, 6)}</Card.Title>
-                            <Card.Subtitle className=" text-white text-xl pl-6 pb-6">{item.arrival_date ? item.arrival_date.slice(-4) : item.delivery_date.slice(-4)}</Card.Subtitle>
-                          </div>
-                          <div>
-                            <Card.Title className=" font-sans text-white  p-6" style={{ display: "flex", alignItems: "center" }}>
-                              <img className="h-10 ml-20" src={locationWhite} alt="" />
-                              <span>{item.import_destination ? item.import_destination : item.export_destination}</span>
-                            </Card.Title>
-                            <Card.Subtitle className=" text-white text-xl pl-6 pb-6 ml-20">{item.container_numbers}</Card.Subtitle>
-                          </div>
-                        </div>
-
-                      </Card.Body>
-                    </Card>
-                  </div>
-                </div>
-              </div>
+              <ShipmentCard item={item} index={index} />
             );
-          })}</div>
+          })}
+        </div>
+
       </div>
     </div>
   )
 };
+
+function ShipmentCard({ item, index }) {
+  return (
+    <Card className="mb-2 w-full 2xl:w-3/5" style={{ backgroundColor: "#217BF4", borderRadius: "10px" }} key={index}>
+      <Card.Body>
+        <div className="grid grid-cols-2 text-white p-4">
+          <div className="flex flex-col justify-center">
+            <Card.Title className="text-5xl justify-start mb-2">{item.arrival_date ? item.arrival_date.slice(0, 6) : item.delivery_date.slice(0, 6)}</Card.Title>
+            <Card.Subtitle className="text-xl justify-start">{item.arrival_date ? item.arrival_date.slice(-4) : item.delivery_date.slice(-4)}</Card.Subtitle>
+          </div>
+          <div className="flex flex-col justify-center">
+            <Card.Title className="flex justify-end mb-2" style={{ alignItems: "center" }}>
+              <img className="h-10 mr-2" src={locationWhite} alt="shipping-icon" />
+              <span>{item.import_destination ? item.import_destination : item.export_destination}</span>
+            </Card.Title>
+            <Card.Subtitle className="text-xl flex justify-end">{item.container_numbers}</Card.Subtitle>
+          </div>
+        </div>
+      </Card.Body>
+    </Card>
+  )
+}
