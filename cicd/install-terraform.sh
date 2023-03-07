d05caab7c564e58b227a0565bf96bf98017ff921
Brayden Leo <46068652+prussianman@users.noreply.github.com>
2023-03-08 04:44:07 +0800

    removed sudo from terraform installation commands

--- cicd/install-terraform.sh
+++ cicd/install-terraform.sh
@@ -4,9 +4,9 @@
 set -eu
 
 #from terraform
-sudo yum install -y yum-utils shadow-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install terraform
+yum install -y yum-utils shadow-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+yum -y install terraform
 
 #confirm installation, will fail if terraform fails to install 
 terraform --version
