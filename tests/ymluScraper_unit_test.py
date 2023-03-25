7c2b8edb9befcba59f8f8297f26295e734215e5b
Brayden Leo <46068652+prussianman@users.noreply.github.com>
2023-03-25 16:32:21 +0800

    add pre-build commands to move files into test folder instead of programmatically importing them - which was not working

--- tests/ymluScraper_unit_test.py
+++ tests/ymluScraper_unit_test.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(1, '/worldgate-track-trace/services/backend/scrapers')
+sys.path.insert(0, '/worldgate-track-trace/services/backend/scrapers')
 import ymlu_scraper
 from ymlu_scraper import app
 import unittest
