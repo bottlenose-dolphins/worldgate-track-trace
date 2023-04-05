f4ee9f37284e4a46fe74be922af65902df64060a
Lim Zhi Hao <70679812+pandaponn@users.noreply.github.com>
2023-04-05 17:46:18 +0800

    retrieve hbl by container number

--- services/backend/core/load_bl_doc.py
+++ services/backend/core/load_bl_doc.py
@@ -1,4 +1,9 @@
+from flask_cors import CORS
 from flask import Flask, jsonify, send_file, request
+from flask_sqlalchemy import SQLAlchemy
+from invokes import invoke_http, invoke_http2
+from os import getenv
+from dotenv import load_dotenv
 import boto3
 
 app = Flask(__name__)
@@ -7,14 +12,24 @@ s3 = boto3.client('s3')
 
 bucket = 'worldgate-tracktrace-docs'
 
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}}, origins="http://localhost:3000",
+     supports_credentials=True, expose_headers="Set-Cookie")
+
+load_dotenv()
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('SQLALCHEMY_DATABASE_URI', None)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+prod = getenv("prod")
+
+db = SQLAlchemy(app)
+
 @app.route('/bl_doc/download', methods=['POST'])
 def download_bl_doc():
     try:
-        # if container, retrieve HBL from container number
-
-        # else, just use HBL
         data = request.get_json()
-        hbl = data["hbl"] + ".pdf"
+        hbl = get_container_hbl(data)
+
         response = s3.get_object(Bucket=bucket, Key=hbl)
         return send_file(response['Body'], mimetype='application/pdf')
     
@@ -30,7 +45,8 @@ def download_bl_doc():
 def embed_bl_doc():
     try:
         data = request.get_json()
-        hbl = data["hbl"] + ".pdf"
+        hbl = get_container_hbl(data)
+
         url = s3.generate_presigned_url(
             ClientMethod='get_object',
             Params={
@@ -50,5 +66,46 @@ def embed_bl_doc():
             }
         ), 500
     
+def get_container_hbl(data):
+    identifier = data["identifier"]
+    identifier_type = data["identifier_type"]
+    direction = data["direction"]
+
+    if identifier_type == "cont":
+        if direction == "import":
+            # find IMPORT_REF_N by CONT_N in IMPORT_REF_CONT
+            data = {
+                "container_number": identifier
+            }
+            import_cont_res = invoke_http2("core_import_cont", "import_cont/import_ref_n", prod, method='POST', json=data)
+            import_ref_n = import_cont_res["data"]["import_ref_n"]
+
+            # find HBL by IMPORT_REF_N in IMPORT
+            data = {
+                "import_ref_n": import_ref_n
+            }
+            import_res = invoke_http2("core_import","import/hbl", prod, method='POST', json=data)
+            hbl = import_res["data"]["hbl"] + ".pdf"
+        
+        elif direction == "export":
+            # find EXPORT_REF_N by CONT_N in EXPORT_REF_CONT
+            data = {
+                "container_number": identifier
+            }
+            export_cont_res = invoke_http2("core_export_cont", "export_cont/export_ref_n", prod, method='POST', json=data)
+            export_ref_n = export_cont_res["data"]["export_ref_n"]
+
+            # find HBL by EXPORT_REF_N in EXPORT
+            data = {
+                "export_ref_n": export_ref_n
+            }
+            export_res = invoke_http2("core_export","export/hbl", prod, method='POST', json=data)
+            hbl = export_res["data"]["hbl"] + ".pdf"
+
+    elif identifier_type == "bl":
+        hbl = identifier + ".pdf"
+    
+    return hbl
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5014, debug=True)
