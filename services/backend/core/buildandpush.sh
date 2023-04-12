1ee857076f4b48047975d295675b04c7dd502bc0
Brayden Leo <46068652+prussianman@users.noreply.github.com>
2023-04-12 08:12:22 +0800

    bug fix: change port number for vessel location ms

--- services/backend/core/buildandpush.sh
+++ services/backend/core/buildandpush.sh
@@ -4,17 +4,17 @@
 #with db uri 
 # images=('user' 'export' 'import' 'export-cont' 'import-cont' 'export-shipment' 'import-shipment' 'complex-scraper' 'prefix' 'vendor-mast' 'view-all' 'subscription', 'unloading-status' 'load-bl-doc', 'scheduler')
 
-images=('load-bl-doc')
+# images=('load-bl-doc')
 
-for image in "${images[@]}"
-do
-  echo "Pushing $image"
+# for image in "${images[@]}"
+# do
+#   echo "Pushing $image"
 
-  docker build -t "tracktrace_repo:core_$image" --file "Dockerfile-$image-prod" --build-arg db=$DB_URI . 
-  docker tag "tracktrace_repo:core_$image" "283879969377.dkr.ecr.ap-southeast-1.amazonaws.com/tracktrace_repo:core_$image"
-  docker push "283879969377.dkr.ecr.ap-southeast-1.amazonaws.com/tracktrace_repo:core_$image"
+#   docker build -t "tracktrace_repo:core_$image" --file "Dockerfile-$image-prod" --build-arg db=$DB_URI . 
+#   docker tag "tracktrace_repo:core_$image" "283879969377.dkr.ecr.ap-southeast-1.amazonaws.com/tracktrace_repo:core_$image"
+#   docker push "283879969377.dkr.ecr.ap-southeast-1.amazonaws.com/tracktrace_repo:core_$image"
 
-done
+# done
 
 # noti complex
 #twilio sid, client
@@ -33,17 +33,16 @@ done
 
 # # //vessel location
 # #gmap, vessel, location
-# images3=('vessel-location')
+images3=('vessel-location')
 
-# # images=('user')
 
-# for image in "${images3[@]}"
-# do
-#   echo "Pushing $image"
-#   docker build -t "tracktrace_repo:core_$image" --file "Dockerfile-$image-prod" --build-arg VESSEL_API_KEY=$VESSEL_API_KEY --build-arg LOCATION_API_KEY=$LOCATION_API_KEY --build-arg GMAPS_API_KEY=$REACT_APP_GMAPS_KEY . 
-#   docker tag "tracktrace_repo:core_$image" "283879969377.dkr.ecr.ap-southeast-1.amazonaws.com/tracktrace_repo:core_$image"
-#   docker push "283879969377.dkr.ecr.ap-southeast-1.amazonaws.com/tracktrace_repo:core_$image"
-# done
+for image in "${images3[@]}"
+do
+  echo "Pushing $image"
+  docker build -t "tracktrace_repo:core_$image" --file "Dockerfile-$image-prod" --build-arg VESSEL_API_KEY="4OrWAN1RwF9yIkFRVK9tE4zjhfNOXiWv6F3629ee" --build-arg LOCATION_API_KEY="1098|NvO69RTGk5mqoTeM5MxboTfq6A2MbV9bUH6UTSHU" --build-arg GMAPS_API_KEY="AIzaSyC24I-F1Ud-LVm7vK89V3MbCu8Ed-NWJ9A" . 
+  docker tag "tracktrace_repo:core_$image" "283879969377.dkr.ecr.ap-southeast-1.amazonaws.com/tracktrace_repo:core_$image"
+  docker push "283879969377.dkr.ecr.ap-southeast-1.amazonaws.com/tracktrace_repo:core_$image"
+done
 
 
 
